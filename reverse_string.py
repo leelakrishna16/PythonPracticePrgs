b7c331cdf183a6b7e848f0098be98b22e35ab100
root <leelaprasad16@gmail.com>
2018-05-11 12:28:24 +0530

    adding python program

---
+++ reverse_string.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+def main():
+   string = 'krishnakarri111'
+   print(string[::-1])
+main()
+def revers(num):
+   rev = num[::-1]
+   return rev
+print(revers('krishnakarri99'))
+
+
+def reverse_string(str):
+  print(str[::-1])
+reverse_string('krishnakaka')
